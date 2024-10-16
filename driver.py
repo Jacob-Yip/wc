f26f7fb3326b7b2bdaa49360385df2121ab4b50b
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-10-16 22:19:45 +0100

    chore: update command format

--- driver.py
+++ driver.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     arguments = sys.argv
 
     assert (len(arguments) == 1 or len(arguments) ==
-            2), f"Invalid command format: python driver.py <file_path> or python ./driver.py"
+            2), f"Invalid command format: python driver.py <file_path> or python ./driver.py expected"
 
     # Create an instance of WC
     if len(arguments) == 2:
@@ -19,25 +19,3 @@ if __name__ == "__main__":
 
     # Print result
     print(wc_data)
-
-
-# if __name__ == "__main__":
-#     arguments = sys.argv
-#     assert len(
-#         arguments) == 2, f"Invalid argument. Expect python3 wc.py <filename>"
-
-#     file_path = arguments[1]
-
-#     wc = WC()
-
-#     if not wc.is_file_path_valid(file_path=file_path):
-#         raise Exception(f"File not found: {file_path}")
-
-#     file = open(file_path, "rt")
-#     lines = file.readlines()
-
-#     line_count = get_line_count(lines=lines)
-#     word_count = get_word_count(lines=lines)
-#     byte_count = get_byte_count(file_path=file_path)
-
-#     print(f"{line_count}    {word_count}    {byte_count}    {file_path}")
