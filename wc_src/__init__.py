6d92fe1f37ba1ed6261a617581c36f20c4bda6d5
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-10-17 16:46:12 +0100

    feat: merge to main

--- wc_src/__init__.py
+++ wc_src/__init__.py
@@ -2,6 +2,3 @@ from .main.python.wc import WC
 from .main.python.wc_data import WCData
 import sys
 import os
-
-print(f"directory_name = {os.path.dirname(os.path.abspath(__file__))}")
-print(f"path = {sys.path}")
