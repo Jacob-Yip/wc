e4c8c890e87b3ba9932f3bcdd5fb138d795bbf36
w45242hy <hin.yip-2@student.manchester.ac.uk>
2024-10-17 13:55:31 +0100

    bugfix: package issue in Python

---
+++ wc/prompt.py
@@ -0,0 +1,28 @@
+"""
+This programme prompts user to provide a file path until he inputs "no"
+
+Command to run the programme
+- python -m wc.prompt (-m treats this file as a module from a package so that we can import WC)
+"""
+
+from wc.main.python.wc import WC
+
+if __name__ == "__main__":
+    again = True
+    while again:
+        try:
+            file_path = str(input("Please enter a valid file path: \n"))
+            wc = WC(file_path=file_path)
+            wc_data = wc.get_wc()
+            print(f"{wc_data}\n")
+
+            again_answer = str(
+                input("Again? Type \"no\" to terminate programme\n"))
+            if again_answer.lower() == "no" or again_answer.lower() == "n":
+                again = False
+                break
+        except Exception as e:
+            print(f"Oops! Something went wrong: {e}")
+            continue
+
+    print(f"Programme terminated gracefully ...")
